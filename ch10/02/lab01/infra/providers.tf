12aa902e0eeef2afbeef165811895d875a6ba73d
jaebok1205 <jaebok1205@gmail.com>
2026-05-21 14:06:16 +0900

    feat: test

--- ch10/02/lab01/infra/providers.tf
+++ ch10/02/lab01/infra/providers.tf
@@ -10,7 +10,7 @@ terraform {
   
   backend "s3" {
     bucket       = "tf-core-tfstate-jaebok1205"
-    key          = "10.02/lab01/terraform.tfstate"
+    key          = "10.02/lab01/infra/terraform.tfstate"
     region       = "ap-northeast-2"
     encrypt      = true
     use_lockfile = true
