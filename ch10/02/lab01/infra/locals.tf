63919256bd0e64e3ee9fdd93d9365855fc5d8643
jaebok1205 <jaebok1205@gmail.com>
2026-05-21 14:59:58 +0900

    local.tf fix

--- ch10/02/lab01/infra/locals.tf
+++ ch10/02/lab01/infra/locals.tf
@@ -6,7 +6,7 @@ locals {
   vpc = {
     name = "main"
 
-    cidr_block           = "10.0.0.0/16"
+    cidr_block           = "172.16.0.0/16"
     enable_dns_support   = true
     enable_dns_hostnames = true
   }
